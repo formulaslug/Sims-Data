e644c820662ca95186a20e0ab65df23e96108e6d
JDB25 <142441598+JDB25@users.noreply.github.com>
2026-01-22 15:57:48 -0800

    added misc graphs and aero now scales with velocity
    Found the most helpful graph was one showing how roll angle changes as speed increases due to teh aero package

--- VDCalculators/springRatesBasic.py
+++ VDCalculators/springRatesBasic.py
@@ -1,9 +1,14 @@
-mass = 293.97 #FS-3 Comp weight + 160lb driver, DOES NOT INCLUDE WEIGHT OF AERO PACKAGE
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+mass = 293.97 #FS-3 Comp weight + 160lb driver, DOES NOT INCLUDE AERO PACKAGE
 weight = 2736  # N
 frontWD = 0.4632
 rearWD = 1 - frontWD
 leftWD = 0.492
-CGHeight = 0.234  # meters #AFFECTED BY AERO PACKAGE AS WELL
+CGHeight = 0.234  # meters
 trackWidth = 1.234
 wheelBase = 1.59
 RCFront = 0.0203
@@ -11,70 +16,190 @@ RCRear = 0.0493
 motionRatioF = 1.006
 motionRatioR = 1.004
 multiplier = 0.00571015 #n/m -> lbf/in
+masterAy= 1
+masterV = 30
 TRG = 0.01524409115   # target roll gradient in rad/g
 print("TRG = ", TRG)
 ###     Aero Loads (N)  ###
-frontWing = 800
-rearWing = 1000     #Taken from total downforce goal
-floorTotal = 200    #with the distribution from the fs-3 
+
+
+def compute_spring_rates(v, ay, trg):
+    frontWing = 0.88888 * v**2
+    rearWing = 1.111 * v**2     #Taken from total downforce goal
+    floorTotal = 0.22222 * v**2    #with the distribution from the fs-3 
                     #aero package CFD
 # Split floor 50 - 50
-frontFloor = floorTotal * 0.5
-rearFloor = floorTotal * 0.5
+    frontFloor = floorTotal * 0.5
+    rearFloor = floorTotal * 0.5
 
-frontAero = frontFloor + frontWing
-rearAero = rearFloor + rearWing
+    frontAero = frontFloor + frontWing
+    rearAero = rearFloor + rearWing
 
-print("Aero Balance (front%) =", frontAero/2000)
+    print("Aero Balance (front%) =", frontAero/2000)
+    ###     Axle Weights    ###
 
-###     Axle Weights    ###
+    frontAW = frontWD * weight + frontAero
+    rearAW = rearWD * weight + rearAero
 
-frontAW = frontWD * weight + frontAero
-rearAW = rearWD * weight + rearAero
+    ###     Roll Moments    ###
 
-###     Roll Moments    ###
+    frontRM = frontAW * (CGHeight - RCFront) * ay
+    rearRM = rearAW * (CGHeight - RCRear) * ay
 
-frontRM = frontAW * (CGHeight - RCFront)
-rearRM = rearAW * (CGHeight - RCRear)
+    ###     Finding Required Roll stiffness     ###
 
-###     Finding Required Roll stiffness     ###
+    frontRS = frontRM / trg
+    rearRS = rearRM / trg
 
-frontRS = frontRM / TRG
-rearRS = rearRM / TRG
+    ###     Moment from one side about CG       ###
+    # roll = Φ
+    # track width = t
+    # Δz = Φ * t/2
+    # wheel rate per side = kw
+    # Force at each wheel:
+    #     F = kw * Φ * t/2
+    # Moment from one side:
+    #     Mside = F * (t/2) = kw * Φ * (t/2)^2
 
-###     Moment from one side about CG       ###
-# roll = Φ
-# track width = t
-# Δz = Φ * t/2
-# wheel rate per side = kw
-# Force at each wheel:
-#     F = kw * Φ * t/2
-# Moment from one side:
-#     Mside = F * (t/2) = kw * Φ * (t/2)^2
+    # (t/2)^2 term
+    halfTrackSq = (trackWidth / 2)**2
 
-# (t/2)^2 term
-halfTrackSq = (trackWidth / 2)**2
+    ###     Wheel Rates    ###
+    # Kphi = 2 * kw * (t/2)^2
+    # kw = Kphi / (2 * (t/2)^2)
 
-###     Wheel Rates    ###
-# Kphi = 2 * kw * (t/2)^2
-# kw = Kphi / (2 * (t/2)^2)
+    frontKW = frontRS / (2 * halfTrackSq)
+    rearKW = rearRS / (2 * halfTrackSq)
 
-frontKW = frontRS / (2 * halfTrackSq)
-rearKW = rearRS / (2 * halfTrackSq)
+    ###     Spring Rates (using motion ratios)     ###
+    # kw = ks * MR^2
+    # ks = kw / MR^2
 
-###     Spring Rates (using motion ratios)     ###
-# kw = ks * MR^2
-# ks = kw / MR^2
+    frontKS = frontKW / (motionRatioF**2)
+    rearKS = rearKW / (motionRatioR**2)
+    return frontKS, rearKS, frontRS, rearRS
+###     Graphs      ###
+def spring_rates_vs_speed_fixed_g():
+    speeds = np.linspace(10, 35, 30)
+    ay = 1.7
+    front_rates = []
+    rear_rates = []
+    for v in speeds:
+        frontKS, rearKS, _, _ = compute_spring_rates(v, ay, TRG)
+        front_rates.append(frontKS / 1000)  # N/mm
+        rear_rates.append(rearKS / 1000)
 
-frontKS = frontKW / (motionRatioF**2)
-rearKS = rearKW / (motionRatioR**2)
+    plt.plot(speeds, front_rates, label="Front")
+    plt.plot(speeds, rear_rates, label="Rear")
+
+    plt.xlabel("Speed (m/s)")
+    plt.ylabel("Spring Rate (N/mm)")
+    plt.title("Required Spring Rate vs Speed @ 1.7g")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+def roll_vs_lateral():
+    g_vals = np.linspace(0.5, 2.5, 30)
+    v = 30  # m/s
+
+    roll_angles = []
+
+    for ay in g_vals:
+        _, _, frontRS, rearRS = compute_spring_rates(v, ay, TRG)
+        Kphi_total = frontRS + rearRS
+        phi = ay / TRG   # radians
+        roll_angles.append(np.degrees(phi))
+
+    plt.plot(g_vals, roll_angles)
+    plt.xlabel("Lateral Acceleration (g)")
+    plt.ylabel("Roll Angle (deg)")
+    plt.title("Roll Angle vs Lateral G @ 30 m/s")
+    plt.grid(True)
+    plt.show()
+
+def roll_angle_vs_speed_calc(speeds, ay, frontKS, rearKS):
+    """
+    Compute roll angle (deg) vs speed for fixed lateral g and fixed springs.
+
+    speeds  : array-like, vehicle speed (m/s)
+    ay      : lateral acceleration (g)
+    frontKS : front spring rate (N/m)
+    rearKS  : rear spring rate (N/m)
+
+    Returns:
+    roll_angles : numpy array of roll angle in degrees
+    """
+
+    # --- Convert springs to roll stiffness ---
+    frontKW = frontKS * motionRatioF**2
+    rearKW  = rearKS  * motionRatioR**2
+
+    halfTrackSq = (trackWidth / 2)**2
+
+    frontRS = 2 * frontKW * halfTrackSq
+    rearRS  = 2 * rearKW  * halfTrackSq
+
+    Kphi_total = frontRS + rearRS
+
+    roll_angles = []
+
+    for v in speeds:
+
+        # --- Aero forces ---
+        frontWing = 0.88888 * v**2
+        rearWing  = 1.111   * v**2
+        floorTotal = 0.22222 * v**2
+
+        frontAero = frontWing + 0.5 * floorTotal
+        rearAero  = rearWing  + 0.5 * floorTotal
+
+        # --- Axle loads ---
+        frontAW = frontWD * weight + frontAero
+        rearAW  = rearWD  * weight + rearAero
+
+        # --- Roll moment ---
+        frontRM = frontAW * (CGHeight - RCFront) * ay
+        rearRM  = rearAW  * (CGHeight - RCRear)  * ay
+
+        M_roll = frontRM + rearRM
+
+        # --- Roll angle ---
+        phi = M_roll / Kphi_total      # radians
+        roll_angles.append(np.degrees(phi))
+
+    return np.array(roll_angles)
+
+def roll_anlge_vs_speed_display(ay):
+    speeds = np.linspace(10, 35, 40)
+    
+
+    frontKS = compute_spring_rates(masterV, masterAy, TRG)[0]   # N/m
+    rearKS  = compute_spring_rates(masterV, masterAy, TRG)[1]   # N/m
+    print(frontKS, rearKS)
+    roll = roll_angle_vs_speed_calc(speeds, ay, frontKS, rearKS)
+    print(frontKS, rearKS)
+
+    plt.plot(speeds, roll)
+    plt.xlabel("Speed (m/s)")
+    plt.ylabel("Roll Angle (deg)")
+    plt.title(f"Roll Angle vs Speed @ {ay}g")
+    plt.grid(True)
+    plt.show()
+#for roll angle vs speed at a set g, call the display one with a constant ay as g's
+
+roll_anlge_vs_speed_display(4)
+frontKW = compute_spring_rates(masterV,masterAy, TRG)[0]
+rearKW = compute_spring_rates(masterV,masterAy, TRG)[1]
+frontKS = compute_spring_rates(masterV,masterAy, TRG)[2]
+rearKS = compute_spring_rates(masterV,masterAy, TRG)[3]
 
 ###     Print results     ###
 
 print("Front Wheel Rate (kN/m):", frontKW/1000)
 print("Rear Wheel Rate (kN/m):", rearKW/1000)
-print("Front Spring Rate (N/mm):", frontKS / 1000)
-print("Rear Spring Rate (N/mm):", rearKS / 1000)
+print("Front Spring Rate (N/m):", frontKS)
+print("Rear Spring Rate (N/m):", rearKS)
 print("Front Spring Rate (lbf/in):", frontKS * multiplier)
 print("Rear Spring Rate (lbf/in):", rearKS * multiplier)
-
