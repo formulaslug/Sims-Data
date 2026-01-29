96d03657e61f299fca9a0bd66c966f718a90baa1
raina chatterjee <rchatte3@ucsc.edu>
2026-01-29 13:36:01 -0800

    Refactor speed comparison plotting and calculations
    Updated speed comparison calculations and plotting.

--- Data/speed-comparisons.py
+++ Data/speed-comparisons.py
@@ -1,15 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from FSLib.IntegralsAndDerivatives import *
+from FSLib.fftTools import *
+from FSLib.AnalysisFunctions import *
 
-x1_values = rpm
-y1_values = time
-x2_values = gps
-y2_values = time
-x3_values = 1/2(rpm+gps)
-y3_values= time
+rpm = "SME_TRQSPD_Speed"
+speed = "VDM_GPS_SPEED"
 
-plt.plot(x1_values, y1_values)
-plt.plot(x2_values, y2_values)
-plt.plot(x3_values, y3_values)
+dt = 0.01  # adjust to your logger
+time = np.arange(df.height) * dt
 
-plt.show
+y1_values = df[rpm].to_numpy() * 12/41*0.2*2*np.pi/60
+y2_values = df[speed].to_numpy()
+y3_values= .5 * (y1_values + y2_values)
+
+plt.plot(time, y1_values, label = "RPM Speed")
+plt.plot(time, y2_values, label = "GPS Speed")
+plt.plot(time, y3_values, label = "Avg Speed")
+
+plt.legend()
+plt.xlabel("Time (s)")
+plt.ylabel("Speed")
+plt.show()
