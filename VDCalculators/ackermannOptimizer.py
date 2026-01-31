792ac622ffee8026f84f076a12b63c98a6845fc1
DanielRhee <danielrheewa@gmail.com>
2026-01-31 13:43:01 -0800

    bruh done

--- VDCalculators/ackermannOptimizer.py
+++ VDCalculators/ackermannOptimizer.py
@@ -2,7 +2,9 @@ import os
 import sys
 import math
 import json
+import numpy
 sys.path.append("../FullVehicleSim/Mech")
+
 from steering import *
 from traction import *
 
@@ -16,15 +18,60 @@ with open('../FullVehicleSim/params.json', 'r') as file:
     del params
 
 
-def calculateAckermann(steerAngle):
-    return (steerAngle, steerAngle)
+def calculateAckermannOther(steerAngle):
+    #steering wheel angle --> steering rack psition --> wheel steer angle (how static ackermann affects wheel angle function)
+    # equations taken from "rack and pinion" section of: https://www.mathworks.com/help/vdynblks/ref/kinematicsteering.html
+    import scipy
+    import numpy
+    import matplotlib.pyplot as plt
+    #global variables
+    #-----------------------------
+    # THIS SCRIPT USES MOSTLY FS-3 VALUES. FS-3 values denoted by [3], any theoretical or FS-4 values denoted by [4]
+    #-----------------------------
+    tw = 1083.3862 #mm (simplified track width from steering axis to steering axis [steering axis is also simplified to be A-arm knuckle to A-arm knuckle])
+    rackRatio = 82.55/248 #[4] mm rack displacement/deg pinion rotation
+    wheelInput = steerAngle * 180/3.141592 #in degrees of steering wheel movement (CW + CCW -)
+    rackShift = 0.0 # mm of movement of the rack from left to right (left is - right is +)
+    l_rack = 292.1 #[4] mm (width of steering rack casing)
+    l_rod = 378.9426 #[3] mm (length of tie rod as left in FS-3 master CAD)
+    d = 109.7788 #[3] mm (plan view distance between front axis and rack. negative because we have a front steer setup)
+    l_arm = 71.628 #[3] mm (length of "steer arm", which is the distance from the center of the upright toe rod pickup to the KPA)
+
+    def rackMovement(): #returns the amount of L-R displacement (in mm) of the steering rack, with the right direction as "positive"
+        rackShift: float = rackRatio*wheelInput
+        return rackShift
+    def calculateAckermann(): #calculates the steer angles of both wheels
+        l1Left = (0.5*(tw-l_rack)) - rackMovement() #l1 is the instantaneous parallel distance from the rack knuckle to steering axis (KPA). 
+        l1Right = (0.5*(tw-l_rack)) + rackMovement()
+        l_nought = (0.5*(tw-l_rack))
+        beta_nought = betaTrigSolver(l_nought) #used to find the initial "beta" geometry to determine the real steer angle at the wheels
+
+        beta_L = betaTrigSolver(l1Left) - beta_nought #additionally, because there is a static "beta" (simply just arm geometry), we must find the difference to find the actual wheel angles
+        beta_R = betaTrigSolver(l1Right) - beta_nought
+    
+        return beta_L, beta_R
+        #return beta_nought, betaTrigSolver(l1Left), betaTrigSolver(l1Right)
+    
+    def betaTrigSolver(l1): #a separate function to solve the big bad trig equation
+        l2 = numpy.sqrt((l1**2) + (d**2)) #l2 is the instantaneous direct distance from rack knuckle to steering axis (KPA)
+        atan = numpy.arctan(d/l1) #first term of the "beta" equation
+
+        num = (l_arm**2) + (l2**2) - (l_rod**2) #just simplifying the calculation of the second term 
+        denom = 2*l_arm*l2
+        frac = num/denom
+        acos = numpy.arccos(frac)
+        beta = (numpy.pi/2) - atan - acos
+        return beta
+        #return frac
+    inTire, outTire = calculateAckermann()
+    return (inTire, outTire)
 
 def calculateSlipAngle(steerAngle, velocity):
     Vx = velocity / math.cos(steerAngle)
     Vy = velocity * math.tan(steerAngle)
     return math.atan(Vy/Vx)
 
-def calculateYawRate(steerAngle, frontCorneringStiffnessDeg, rearCorneringStiffnessDeg):
+def calculateYawRate(steerAngle, frontCorneringStiffnessDeg, rearCorneringStiffnessDeg, speed, stepSteerInput):
     CF = frontCorneringStiffnessDeg * 180 / np.pi
     CR = rearCorneringStiffnessDeg * 180 / np.pi
     a = 0.853506
@@ -44,57 +91,76 @@ def calculateYawRate(steerAngle, frontCorneringStiffnessDeg, rearCorneringStiffn
     return r_inf
 
 def solver(minSteer, maxSteer, minVelocity, maxVelocity):
-    carMass = 277 / 4 
+    carMass = [277 / 4, 277 / 4 , 277 / 4 , 277 / 4 ]
     res = []
-    for steer in range (minSteer, maxSteer, 0.1):
+    for steer in np.arange(minSteer, maxSteer, 0.1):
         currLine = []
-        for velocity in range(minVelocity, maxVelocity, 1):
-            inTire, outTire = calculateAckermann(steer)
+        for velocity in np.arange(minVelocity, maxVelocity, 1):
+            inTire, outTire = calculateAckermannOther(steer)
             inCorneringAngleSlip = calculateSlipAngle(inTire, velocity)
             outCorneringAngleSlip = calculateSlipAngle(outTire, velocity)
-            inCorneringStiff = getCorneringStiffness(carMass,
-                                                    inCorneringAngleSlip,
-                                                    0.15,
-                                                    velocity,
-                                                    80,
-                                                    40,
-                                                    parameters,
-                                                    magic)
-           outCorneringStiff = getCorneringStiffness(carMass,
-                                                    outCorneringAngleSlip,
-                                                    0.15,
-                                                    velocity,
-                                                    80,
-                                                    40,
-                                                    parameters,
-                                                    magic)
-            netFrontCorneringStiffness = inCorneringStiffness + outCorneringStiffness
-            netRearCornerningStiffness = -140 # lol who knows bruh
+            inCorneringStiff = getCorneringStiffness(carMass, (inCorneringAngleSlip, 0), 0.15, velocity, 80, 40, Parameters, Magic)[0]
+            outCorneringStiff = getCorneringStiffness(carMass, (outCorneringAngleSlip, 0),  0.15, velocity, 80, 40, Parameters, Magic)[0]
+            netFrontCorneringStiffness = (inCorneringStiff + outCorneringStiff)/2
+            netRearCornerningStiffness = -70 # lol who knows bruh
 
-            yawRate = calculateYawRate(steer, inCornerningStiff, outCorneringStiff)
+            yawRate = calculateYawRate(steer, netFrontCorneringStiffness, netRearCornerningStiffness, velocity, steer) * -1
             currLine.append(yawRate)
         res.append(currLine)
     return res
 
-        
-if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-    minSteer = 0
-    maxSteer = 1.8
-    minVelocity = 1
-    maxVelocity = 30 
 
-    yaw_data = solver(minSteer, maxSteer, minVelocity, maxVelocity)
-    
-    steering_angles = np.arange(minSteer, maxSteer, 0.1)
-    velocities = np.arange(minVelocity, maxVelocity)
-    S, V = np.meshgrid(steering_angles, velocities)
-    YawRate = np.array(yaw_data)
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot_surface(S, V, YawRate, cmap='viridis')
-    ax.set_xlabel('Steering Angle (degrees)')
-    ax.set_ylabel('Velocity (m/s)')
-    ax.set_zlabel('Yaw Rate (rad/s)')
-    plt.show()
+# --- solver inputs ---
+minSteer = 0
+maxSteer = 1.75
+minVelocity = 10
+maxVelocity = 30
+
+# --- run solver ---
+yaw_rates = solver(minSteer, maxSteer, minVelocity, maxVelocity)
+
+# --- build coordinate lists for trisurf ---
+steer_vals = np.arange(minSteer, maxSteer, 0.1)
+velocity_vals = np.arange(minVelocity, maxVelocity, 1)
+
+S = []
+V = []
+Z = []
+
+for i, steer in enumerate(steer_vals):
+    for j, velocity in enumerate(velocity_vals):
+        S.append(steer)
+        V.append(velocity)
+        Z.append(yaw_rates[i][j])
+
+S = np.array(S)
+V = np.array(V)
+Z = np.array(Z)
+
+# --- plot ---
+fig = plt.figure(figsize=(12, 8))
+ax = fig.add_subplot(111, projection='3d')
+
+surf = ax.plot_trisurf(
+    V,              # x-axis
+    S,              # y-axis
+    Z,              # z-axis
+    cmap='viridis',
+    linewidth=0.2,
+    antialiased=True
+)
+
+ax.set_xlabel("Velocity")
+ax.set_ylabel("Steering Angle")
+ax.set_zlabel("Yaw Rate")
+ax.set_title("Yaw Rate vs Steering Angle and Velocity")
+
+fig.colorbar(surf, ax=ax, shrink=0.5, aspect=10, label="Yaw Rate")
+
+plt.tight_layout()
+plt.show()
+
