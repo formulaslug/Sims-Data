5bda9d4967f350f6f90dd3ca0ae68f0a79225d41
DanielRhee <danielrheewa@gmail.com>
2026-01-31 12:19:02 -0800

    setup

--- VDCalculators/ackermannOptimizer.py
+++ VDCalculators/ackermannOptimizer.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import math
 import json
 sys.path.append("../FullVehicleSim/Mech")
 from steering import *
@@ -12,3 +13,14 @@ with open('../FullVehicleSim/params.json', 'r') as file:
     Magic = params["Magic"]
     Parameters = params["Parameters"]
     del params
+
+
+def calculateAckermann(steerAngle):
+    return (steerAngle, steerAngle)
+
+def calculateSlipAngle(steerAngle, velocity):
+    Vx = velocity / math.cos(steerAngle)
+    Vy = velocity * math.tan(steerAngle)
+    return math.atan(Vy/Vx)
+
+def calculateYawRate(steerAngle
