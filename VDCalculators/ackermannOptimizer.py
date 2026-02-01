37d1f74705d8b44a6a210764613439dbfcc6e9e0
calebshin1 <86570471+calebshin1@users.noreply.github.com>
2026-01-31 19:00:33 -0800

    workday finishing touches

--- VDCalculators/ackermannOptimizer.py
+++ VDCalculators/ackermannOptimizer.py
@@ -5,13 +5,11 @@ import json
 import numpy
 sys.path.append("../FullVehicleSim/Mech")
 
-from steering import *
-from traction import *
 
 import json
 magic:dict
 parameters:dict
-with open('../FullVehicleSim/params.json', 'r') as file:
+with open('../../FullVehicleSim/params.json', 'r') as file:
     params = json.load(file)
     Magic = params["Magic"]
     Parameters = params["Parameters"]
