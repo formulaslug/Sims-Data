687a68529c8a0393eb3b6e65b2a542837f202467
aaron345678 <aaronchun305@gmail.com>
2026-02-24 12:31:21 -0800

    TCP Stream only sends initial data :((

--- FullVehicleSim/TCP_Socket_Client_Test.py
+++ FullVehicleSim/TCP_Socket_Client_Test.py
@@ -1,12 +1,14 @@
 import socket
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 80        # The port used by the server
+PORT = 5000        # The port used by the server
 
+    
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.sendall(b'Hello, world')
-data = s.recv(1024)
-
+s.sendall(b'start protocol')
+data = s.recv(1024) ##data gets recieved
 print(f"Received {data.decode()!r}")
-s.sendall(b'end protocol')
+
+s.sendall(b'end protocol') ##why doesnt this get run?
+
--- FullVehicleSim/TCP_Socket_Server.py
+++ FullVehicleSim/TCP_Socket_Server.py
@@ -2,26 +2,33 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 HOST_IP = "127.0.0.1" #Made to only use on local machine (for now)
-PORT = 80
+PORT = 5000
 def setup():
     sock.bind((HOST_IP,PORT))
     sock.listen(5)
 
 def closeSocket():
-    sock.shutdown()
+    sock.shutdown(socket.SHUT_RDWR)
     sock.close()
 
 def run_socket():
     setup()
     ghost_var = "Velocity data will be here"
+    var1 = 0
+    
     while True:
-        clientsocket, address = sock.accept()
         print(address)
+        clientsocket, address = sock.accept()
         data = clientsocket.recv(1024).decode()
+         ##Code gets stuck on here since it blocks?
+        print('Recieved Data: ' + data)
         if(data == None or data == 'end protocol'):
             closeSocket()
             break
-        clientsocket.send(ghost_var.encode("utf-8"))
+        if(var1 == 0):
+            clientsocket.send(ghost_var.encode("utf-8"))
+            print('sent fake velocity data')
+            var1 +=1
     return 0
 
 
