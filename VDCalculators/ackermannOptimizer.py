f856cfcddaa1c635844e62579790f777d220120e
calebshin1 <86570471+calebshin1@users.noreply.github.com>
2026-03-03 22:28:39 -0800

    adding prntln for steering angles

--- VDCalculators/ackermannOptimizer.py
+++ VDCalculators/ackermannOptimizer.py
@@ -77,6 +77,7 @@ def calculateAckermannOther(steerAngle): #input in rad
 
         return leftSteerAngle, rightSteerAngle #OUTPUT WILL ALSO BE IN RAD
     inTire, outTire = calculateSteerAngles(steerAngle)
+    print(f"inside: {numpy.rad2deg(inTire)} outside: {numpy.rad2deg(outTire)}")
     return (inTire, outTire)
 def getSteeringState():
     return {
@@ -214,20 +215,12 @@ minSteer = -1.75 #changed to 0.1 because graph was throwing some crazy values
 maxSteer = 1.75
 minVelocity = 10
 maxVelocity = 30
-<<<<<<< HEAD
-fixedSteer = 1.65 #in rad btw
-=======
 fixedSteer = 1.5 #in rad btw
->>>>>>> 1212fb06456da385b23711a4a630088d69820707
 
 # -run solver (USG graph)
 steer_vals = np.arange(fixedSteer)
 velocity_vals = np.arange(minVelocity, maxVelocity, 1)
-<<<<<<< HEAD
-rack_offsets = np.arange(-80, 81, 20)  #steps would be in mm
-=======
 rack_offsets = np.arange(-25, 25, 5)  #steps would be in mm
->>>>>>> 1212fb06456da385b23711a4a630088d69820707
 base_state = getSteeringState()
 
 
