0c8f1cbebf550dbc84fb46d3c559fbcd5d62f982
aaronC34 <aaronchun305@gmail.com>
2026-04-06 16:34:19 -0700

    somewhat cooked flows back and forth

--- FullVehicleSim/TCP_Socket_Client_Test.py
+++ FullVehicleSim/TCP_Socket_Client_Test.py
@@ -2,15 +2,29 @@ import socket
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 5000        # The port used by the server
-
-    
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-s.sendall(b'start protocol.R?R')
-data = s.recv(1024) ##data gets recieved
-print(f"Received {data.decode()!r}")
 
-s.sendall(b'I am still listening!.R?R') 
-data = s.recv(1024)
-print(f"Received {data.decode()!r}")
-s.sendall(b'end protocol')
+def setup():
+    s.connect((HOST, PORT))
+    s.sendall(b'start protocol')
+def close():
+    s.shutdown(socket.SHUT_RDWR)
+    s.close()
+def recieveData():
+    data = s.recv(1024) ##data gets recieved
+    word = data.decode()
+    print(f"Received " + word)
+    return word
+def sendData(data):
+    s.send(data)
+def constantly_connect():
+    setup()
+    while True:
+        data = recieveData()
+        if(data == "end protocol"):
+            sendData('end protocol')
+            close()
+            break
+        strIn = input("Sending Data from client: ")
+        sendData(strIn.encode("utf-8"))
+constantly_connect()
--- FullVehicleSim/TCP_Socket_Server.py
+++ FullVehicleSim/TCP_Socket_Server.py
@@ -13,28 +13,27 @@ def closeSocket():
 def is_connected_to_client():
     try:  
         sock.send(b'some data')
-
+        clientsocket, address = sock.accept()
+        data = clientsocket.recv(1024).decode()
+        if(data != None):
+            print('Server is connected to client!')
+            return True
     except:
         return False
-    print('Server is connected to client!')
-    return True
 def run_socket():
     setup()
     ghost_var = "Velocity data will be here"
     var1 = 0
     clientsocket, address = sock.accept()
-    while is_connected_to_client():
+    while True:
         print(address)
         data = clientsocket.recv(1024).decode()
-         ##Code gets stuck on here since it blocks? 
-        print('Recieved Data: ' + data)
+        print('Recieved Data at server: ' + data)
         if(data == None or data == 'end protocol'):
             closeSocket()
             break
-        arr = data.split('.')
-        if(arr[1] == 'R?R'):
-            phrase = str(input("Input something here: "))
-            clientsocket.send(phrase.encode("utf-8"))
+        phrase = str(input("Input something here: "))
+        clientsocket.send(phrase.encode("utf-8"))
     return 0
 
 
