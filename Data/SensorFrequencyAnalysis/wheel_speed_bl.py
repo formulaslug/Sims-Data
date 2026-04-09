2f255cbf1dfaae6fc700ca4f72e411f0b16e6395
aanyajain6 <aanyajain0602@gmail.com>
2026-04-08 23:31:27 -0700

    Create wheel_speed_bl.py

---
+++ Data/SensorFrequencyAnalysis/wheel_speed_bl.py
@@ -0,0 +1,30 @@
+import scipy
+import numpy as np
+import polars as pl
+import matplotlib.pyplot as plt
+
+df = pl.read_parquet('/Users/aanyajain/Documents/GitHub/fs-data/FS-3/11222025/11222025_21.parquet')
+df = df.fill_null(strategy="forward").fill_null(strategy="backward")
+signal = df["TPERIPH_BL_DATA_WHEELSPEED"].to_numpy()
+signal = signal[signal > 0]
+fft = scipy.fft.fft(signal)
+freqs = scipy.fft.fftfreq(len(signal), d=0.01)
+attempts = [9.5]
+ffts = []
+iffts = []
+for i, attempt in enumerate(attempts):
+    ffts.append(fft.copy())
+    ffts[-1][np.log(np.abs(fft)) < attempt] = 0
+    iffts.append(scipy.fft.ifft(ffts[-1]))
+plt.plot(signal, label="Real")
+for ifft, attempt in zip(iffts, attempts):
+    plt.plot(ifft.real, label=f"Attempt {attempt}")
+plt.xlabel("Time")
+plt.ylabel("Amplitude")
+plt.legend()
+plt.show()
+plt.scatter(freqs, np.log(np.abs(fft)), s=0.25)
+plt.xlabel("Frequency (Hz)")
+plt.ylabel("Magnitude")
+plt.title("Wheel Speed BL - Frequency Spectrum")
+plt.show()
