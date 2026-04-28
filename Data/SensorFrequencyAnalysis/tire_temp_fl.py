18757989e9df24e181b2dd15e2ea69dad23597a6
aanyajain6 <aanyajain0602@gmail.com>
2026-04-27 19:17:18 -0700

    Update tire_temp_fl.py

--- Data/SensorFrequencyAnalysis/tire_temp_fl.py
+++ Data/SensorFrequencyAnalysis/tire_temp_fl.py
@@ -6,15 +6,26 @@ import matplotlib.pyplot as plt
 df = pl.read_parquet('/Users/aanyajain/Documents/GitHub/fs-data/FS-3/11222025/11222025_1.parquet')
 df = df.fill_null(strategy="forward").fill_null(strategy="backward")
 signal = df["TPERIPH_FL_TIRETEMP_1"].to_numpy()
+
 fft = scipy.fft.fft(signal)
 freqs = scipy.fft.fftfreq(len(signal), d=0.01)
-attempts = [7]
+
+attempts = [6, 7, 8]
 ffts = []
 iffts = []
 for i, attempt in enumerate(attempts):
     ffts.append(fft.copy())
     ffts[-1][np.log(np.abs(fft)) < attempt] = 0
-    iffts.append(scipy.fft.ifft(ffts[-1]))
+    ifft = scipy.fft.ifft(ffts[-1])
+    iffts.append(ifft)
+
+RMSs = [np.sqrt(np.mean((ifft.real - signal)**2)) for ifft in iffts]
+plt.plot(attempts, RMSs)
+plt.xlabel("Attempt")
+plt.ylabel("RMS")
+plt.title("Tire Temp FL - RMS vs Attempt")
+plt.show()
+
 plt.plot(signal, label="Real")
 for ifft, attempt in zip(iffts, attempts):
     plt.plot(ifft.real, label=f"Attempt {attempt}")
@@ -22,6 +33,7 @@ plt.xlabel("Time")
 plt.ylabel("Amplitude")
 plt.legend()
 plt.show()
+
 plt.scatter(freqs, np.log(np.abs(fft)), s=0.25)
 plt.xlabel("Frequency (Hz)")
 plt.ylabel("Magnitude")
