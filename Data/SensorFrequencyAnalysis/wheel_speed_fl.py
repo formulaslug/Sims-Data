cd5b80d77b5947e7e90d9ba1813e85ca0539704d
aanyajain6 <aanyajain0602@gmail.com>
2026-04-27 19:20:35 -0700

    Update wheel_speed_fl.py

--- Data/SensorFrequencyAnalysis/wheel_speed_fl.py
+++ Data/SensorFrequencyAnalysis/wheel_speed_fl.py
@@ -7,15 +7,26 @@ df = pl.read_parquet('/Users/aanyajain/Documents/GitHub/fs-data/FS-3/11222025/11
 df = df.fill_null(strategy="forward").fill_null(strategy="backward")
 signal = df["TPERIPH_FL_DATA_WHEELSPEED"].to_numpy()
 signal = signal[signal > 0]
+
 fft = scipy.fft.fft(signal)
 freqs = scipy.fft.fftfreq(len(signal), d=0.01)
-attempts = [9]
+
+attempts = [8, 9, 10]
 ffts = []
 iffts = []
 for i, attempt in enumerate(attempts):
     ffts.append(fft.copy())
     ffts[-1][np.log(np.abs(fft)) < attempt] = 0
-    iffts.append(scipy.fft.ifft(ffts[-1]))
+    ifft = scipy.fft.ifft(ffts[-1])
+    iffts.append(ifft)
+
+RMSs = [np.sqrt(np.mean((ifft.real - signal)**2)) for ifft in iffts]
+plt.plot(attempts, RMSs)
+plt.xlabel("Attempt")
+plt.ylabel("RMS")
+plt.title("Wheel Speed FL - RMS vs Attempt")
+plt.show()
+
 plt.plot(signal, label="Real")
 for ifft, attempt in zip(iffts, attempts):
     plt.plot(ifft.real, label=f"Attempt {attempt}")
@@ -23,6 +34,7 @@ plt.xlabel("Time")
 plt.ylabel("Amplitude")
 plt.legend()
 plt.show()
+
 plt.scatter(freqs, np.log(np.abs(fft)), s=0.25)
 plt.xlabel("Frequency (Hz)")
 plt.ylabel("Magnitude")
